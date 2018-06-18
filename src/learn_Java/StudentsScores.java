4cacb2feba8cffed3eeefee5774e3033821402b2
rraihan <dialraihan@gmail.com>
2018-06-18 02:30:27 -0400

    First Commit

---
+++ src/learn_Java/StudentsScores.java
@@ -0,0 +1,23 @@
+package learn_Java;
+
+public class StudentsScores {
+
+	public static void main(String[] args) {
+int result =661;
+		
+		if 
+		(result >= 660 || result == 660) {
+			System.out.println(" 'Congrats' "+"your passing score is First Divition.\n\t\tYou are in Dean List.");
+		}
+		else
+			if (result < 660 && result >=550  ) {
+			System.out.println("You are in Second Divition.");
+			}
+		else {
+			System.out.println("You Barely Pass the Exam. " + "Study Hard.");
+		
+		}
+
+	}
+
+}
